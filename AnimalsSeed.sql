186146a6203b5be49d812eb449bb455700c77436
Victor Rono <v.rono@icloud.com>
2019-03-30 16:21:17 -0500

    seeded data for Animals table in SQL Server

---
+++ AnimalsSeed.sql
@@ -0,0 +1,19 @@
+INSERT INTO dbo.Animals VALUES ('Bruce',1,52,3,1,'relaxed',1,0,'male', 'adopted', 1);
+
+INSERT INTO dbo.Animals VALUES ('Sally',2,15,5,2,'relaxed',1, 0,'female', 'adopted', 1);
+
+INSERT INTO dbo.Animals VALUES ('Pluto',3,2,2,4,'calm',1,1,'male', 'adopted', 2);
+
+INSERT INTO dbo.Animals VALUES ('Rolo',4,2,19,3,'involved',1,0,'female', 'adopted', 1);
+
+INSERT INTO dbo.Animals VALUES ('Bash',9,13,5,5,'mean',0,0,'male', 'not adopted', 2);
+
+INSERT INTO dbo.Animals VALUES ('Moose',5,3,3,5,'nice',0,1,'male', 'adopted', 2);
+
+INSERT INTO dbo.Animals VALUES ('Mars',8,5,6,4,'stand-offish',0,0,'female', 'not adopted', 1);
+
+INSERT INTO dbo.Animals VALUES ('Star',10,2,1,5,'gentle',1,1,'female', 'not adopted', 2);
+
+INSERT INTO dbo.Animals VALUES ('Vamose',6,3,5,5,'nice',1,1,'male', 'adopted', 1);
+
+INSERT INTO dbo.Animals VALUES ('Bugs Bunny',7,4,2,5,'cuddly',1,1,'female', 'not adopted', 1);
