b7fde98413f4ae0dc5a768bde2f8dd737d58333c
Victor Rono <v.rono@icloud.com>
2019-03-30 16:11:16 -0500

    seeded data for categories table in SQL Server

---
+++ CategoriesSeed.sql
@@ -0,0 +1,10 @@
+INSERT INTO dbo.Categories VALUES ('Dog');
+INSERT INTO dbo.Categories VALUES ('Cat');
+INSERT INTO dbo.Categories VALUES ('Lizard');
+INSERT INTO dbo.Categories VALUES ('Bird');
+INSERT INTO dbo.Categories VALUES ('Rodent');
+INSERT INTO dbo.Categories VALUES ('Ferret');
+INSERT INTO dbo.Categories VALUES ('Rabbit');
+INSERT INTO dbo.Categories VALUES ('Snake');
+INSERT INTO dbo.Categories VALUES ('Monkey');
+INSERT INTO dbo.Categories VALUES ('Hamster');
