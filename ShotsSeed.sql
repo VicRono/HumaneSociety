a896ca40ac76df6dabe9e82c0f639848fd03a150
Victor Rono <v.rono@icloud.com>
2019-03-30 16:20:00 -0500

    seeded data for Shots table in SQL Server

---
+++ ShotsSeed.sql
@@ -0,0 +1,5 @@
+INSERT INTO dbo.Shots VALUES ('Rabies');
+INSERT INTO dbo.Shots VALUES ('Distemper');
+INSERT INTO dbo.Shots VALUES ('Dewormer');
+INSERT INTO dbo.Shots VALUES ('Parvovirus');
+INSERT INTO dbo.Shots VALUES ('Leukemia');
