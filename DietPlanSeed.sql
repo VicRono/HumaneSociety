8522ba369950c4260d25f1b72659ebcbd1c5493d
Victor Rono <v.rono@icloud.com>
2019-03-30 16:14:33 -0500

    seeded data for DietPlan table in SQL Server

---
+++ DietPlanSeed.sql
@@ -0,0 +1,5 @@
+INSERT INTO dbo.DietPlans VALUES ('Canine', 'Dry', 3);
+INSERT INTO dbo.DietPlans VALUES ('Feline', 'Wet', 2);
+INSERT INTO dbo.DietPlans VALUES ('Avian', 'Seed', 1);
+INSERT INTO dbo.DietPlans VALUES ('Reptile', 'Fresh', 2);
+INSERT INTO dbo.DietPlans VALUES ('Mammal', 'Fresh', 1);
